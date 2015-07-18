6e79a5794b848ed4803071adbcbd6cbc02c0015b
Marek Labos <marek@specle.net>
2015-07-18 13:49:44 +0100

    fix lint errors

--- www/javascripts/app.js
+++ www/javascripts/app.js
@@ -1,9 +1,29 @@
-/*global $, window, specle, doc */
+/*global $, window, Autodesk, doc */
 
 (function () {
     'use strict';
 
-    var doc = $(document);
+    var doc = $(document),
+
+        adn_config = {
+            environment : 'AutodeskProduction', // 'AutodeskProduction' | 'AutodeskStaging'
+            viewerType: 'Viewer3D'
+        },
+
+        scenes_names = ['back'],
+
+        viewer = Autodesk.ADN.Toolkit.Viewer,
+        tokenurl = '/www/data/token.json',
+        urn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6YWRuLWdhbGxlcnktdHJ4L2NiN2ItNjVmYS0xYjZmLWUzODctYzkzMi5mM2Q=';
+
+
+    function onViewerInitialized(viewer) {
+        console.log('Viewer initialized', viewer);
+    }
+
+    function onError(error) {
+        console.log(error);
+    }
 
     function fullscreen(element) {
         if (element.requestFullscreen) {
@@ -17,10 +37,32 @@
         }
     }
 
-    // $(function () {
-    // });
+    function initAdnViewerManager(tokenurl, holder, config) {
+        return new viewer.AdnViewerManager(tokenurl, holder, config);
+    }
+
+    function initScene(holder) {
+        var adnViewerMng = initAdnViewerManager(tokenurl, holder, adn_config);
+
+        adnViewerMng.loadDocument(urn, onViewerInitialized, onError);
+        return adnViewerMng;
+    }
+
+    function initScenes(scenes_names, holder) {
+        return scenes_names.map(function (name) {
+            return initScene(holder.getElementsByClassName(name + '-scene')[0]);
+        });
+    }
 
     doc.on('click', '#fullscreen-anchor', function () {
-        fullscreen($('#scene-holder').get(0));
+        fullscreen(
+            document.getElementById('scene-holder')
+        );
+    });
+
+    $(function () {
+        var holder = document.getElementById('scene-holder');
+
+        initScenes(scenes_names, holder);
     });
 }());
